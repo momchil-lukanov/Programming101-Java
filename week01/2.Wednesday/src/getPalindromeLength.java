c39da376271cc5c90938fd7e2ad12917aca74f38
Momchil Lukanov <momchil.lukanov@gmail.com>
2015-11-30 11:32:26 +0200

    getPalindromeLenght added task.

--- week01/2.Wednesday/src/getPalindromeLength.java
+++ week01/2.Wednesday/src/getPalindromeLength.java
@@ -1,41 +1,43 @@
 import java.util.Vector;
 
+import com.sun.org.apache.xpath.internal.operations.Equals;
+
 public class getPalindromeLength {
 
 	public static int getPalindromeLength(String input) {
-		
+
 		Vector<String> temp = new Vector<>();
 		String word = "";
 		int counter = 0;
-		
+
 		for (int i = 0; i < input.length(); i++) {
-			if (input.charAt(i) != '*') {
+			if (input.charAt(i) == '*') {
+				counter += 1;
+				temp.add(word);
+				word = "";
+			} else {
 				if (counter == 0) {
 					word = input.charAt(i) + word;
 				} else {
 					word += input.charAt(i);
 				}
-			} else {
-				counter += 1;
-				temp.add(word);
-				word = "";
 			}
 		}
-		
+		temp.add(word);
 		counter = 0;
-		
-		for (int i = 0; i < temp.size(); i++) {
-			for (int j = 0; j < temp.get(j).length(); j++) {
-				
+
+		for (int i = 0; i < temp.get(0).length(); i++) {
+			Character x = temp.get(0).charAt(i);
+			Character y = temp.get(1).charAt(i);
+			if (x.equals(y)) {
+				counter += 1;
 			}
 		}
-		
 		return counter;
 	}
-	
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String a = "daz*zad";
+		System.out.println(getPalindromeLength(a));
 	}
-
 }
