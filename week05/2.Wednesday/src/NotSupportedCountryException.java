5b9262466a2bc0f87c39674202bf2761f774fcac
Momchil Lukanov <momchil.lukanov@gmail.com>
2016-01-06 19:11:01 +0200

    updated week05

---
+++ week05/2.Wednesday/src/NotSupportedCountryException.java
@@ -0,0 +1,5 @@
+public class NotSupportedCountryException extends Exception {
+	public NotSupportedCountryException(String message) {
+		super(message);
+	}
+}
---
+++ week06/3.Wednesday/src/FunnyArraySorter.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Array;
+import java.util.List;
+
+public class FunnyArraySorter {
+	
+	public Array array;
+	
+	public FunnyArraySorter(int number) {
+		
+	}
+	
+	public List reverseSort(List list){
+		
+		
+		
+		return list;
+		
+	}
+}
